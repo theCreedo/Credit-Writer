ce46b1f9c01825b69957a1a5708cbde05847f339
Roberto Hernandez <hernandezr@smu.edu>
2017-03-15 00:46:17 -0500

    fixed php file

--- savejson.php
+++ savejson.php
@@ -1,15 +1,26 @@
 <?php
    $json = $_POST['json'];
 
-   /* sanity check */
-   if (json_decode($json) != null)
-   {
+   // $json = "{something: true}";
+
+   // echo $json;
+
+   // echo json_decode($json);
+
+   // /* sanity check */
+   // if (json_decode($json) != null)
+   // {
+   //   echo "it worked";
      $file = fopen('info.json','w+');
+     // fwrite($file, $json);
      fwrite($file, $json);
+
      fclose($file);
-   }
-   else
-   {
-     // user has posted invalid JSON, handle the error 
-   }
+   // }
+   // else
+   // {
+   //  echo 'console.log($json)';
+   //   // user has posted invalid JSON, handle the error 
+   // }
+
 ?>
