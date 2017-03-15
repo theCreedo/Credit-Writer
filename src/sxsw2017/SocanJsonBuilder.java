8c3accec3f74dc4ff7ebf001243795eca07ff72b
LilyEssence <lily.essence.two@gmail.com>
2017-03-15 06:35:31 -0500

    added method for creation of json with only a couple specific fields and an example use case

--- src/sxsw2017/SocanJsonBuilder.java
+++ src/sxsw2017/SocanJsonBuilder.java
@@ -1,5 +1,6 @@
 package sxsw2017;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -26,22 +27,19 @@ class Composition{
 }
 
 public class SocanJsonBuilder {
-	public static void main (String [] cheese){
-		System.out.println("Hello, World!");
-
-		//TODO: Populate these!//
+	public static JsonObject buildJson(String artist_nam, String cit, String stat, ArrayList<String> songs){
 		String marie_no = "9999999";
 		String canadian_performance = "C";
 		String type_of_program = "SC";
-		String artist_name = "John Doe";
+		String artist_name = artist_nam;
 		String proof_type = "contract";
 		String date_of_program = "2017/12/10";
 		String venue = "SOCAN";
 		String venuetype = "cfs";
 		String venuecapacity = "15";
 		String street1 = "41 Valleybrook";
-		String city = "Toronto";
-		String province = "ON";
+		String city = cit;
+		String province = stat;
 		String postal_code = "m3b 2s6";
 		String country = "can";
 		String venue_phone = "4164458700";
@@ -55,8 +53,9 @@ public class SocanJsonBuilder {
 		String promoter_country =  "CAN";
 		String promoter_telephone = "4164458700";
 		Set <Composition> allCompositions = new HashSet<Composition>();		
-		allCompositions.add(new Composition("SOCAN", "John Doe"));
-		allCompositions.add(new Composition("HACKATHON", "John Doe"));		
+		for (String song : songs){
+			allCompositions.add(new Composition(song, artist_nam));	
+		}
 		//End of populating things ^^^^^//
 
 		JsonBuilderFactory factory = Json.createBuilderFactory(null);
@@ -99,7 +98,91 @@ public class SocanJsonBuilder {
 				.add("PROMOTER_TELEPHONE", promoter_telephone)
 				.add("compositions", arrayBuilder)		    		  
 				.build();
+		return value;
+	}
+	
+	public static void main (String [] cheese){
+		System.out.println("Hello, World!");
+		
+		ArrayList<String> mockSongs = new ArrayList<String>();
+		mockSongs.add("LE FIRST BEAUTIFUL SONG");
+		mockSongs.add("LE SECOND BEAUTIFUL SONG");
+		
+		JsonObject testJson = buildJson("LE ARTIST", "LE CITY", "LE STATE", mockSongs);
 
-		System.out.println(value.toString());
+		System.out.print(testJson.toString());
+		
+		//TODO: Populate these!//
+//		String marie_no = "9999999";
+//		String canadian_performance = "C";
+//		String type_of_program = "SC";
+//		String artist_name = "John Doe";
+//		String proof_type = "contract";
+//		String date_of_program = "2017/12/10";
+//		String venue = "SOCAN";
+//		String venuetype = "cfs";
+//		String venuecapacity = "15";
+//		String street1 = "41 Valleybrook";
+//		String city = "Toronto";
+//		String province = "ON";
+//		String postal_code = "m3b 2s6";
+//		String country = "can";
+//		String venue_phone = "4164458700";
+//		String venue_website = "www.socan.ca";
+//		String performance_time=  "1 : 30 pm";
+//		String promoter= "SOCAN";
+//		String promoter_street1 = "41 Valleybrook";
+//		String promoter_city= "Toronto";
+//		String promoter_province = "ON";
+//		String promoter_postal_code = "M3B2S6";
+//		String promoter_country =  "CAN";
+//		String promoter_telephone = "4164458700";
+//		Set <Composition> allCompositions = new HashSet<Composition>();		
+//		allCompositions.add(new Composition("SOCAN", "John Doe"));
+//		allCompositions.add(new Composition("HACKATHON", "John Doe"));		
+//		//End of populating things ^^^^^//
+//
+//		JsonBuilderFactory factory = Json.createBuilderFactory(null);
+//		JsonObjectBuilder compBuilder = factory.createObjectBuilder();
+//		JsonArrayBuilder arrayBuilder = factory.createArrayBuilder();
+//
+//		for (Composition comp : allCompositions){
+//			arrayBuilder.add(compBuilder.add("ORIGINAL_TITLE", comp.original_title)
+//					.add("COMPOSER", comp.composer));
+//		}
+//
+//		Map<String, Object> config = new HashMap<String, Object>();
+//		//if you need pretty printing
+//		config.put("javax.json.stream.JsonGenerator.prettyPrinting", Boolean.valueOf(true));
+//
+//		JsonObject value = factory.createObjectBuilder()
+//				.add("MARIE_NO", marie_no)
+//				.add("CANADIAN_PERFORMANCE", canadian_performance)
+//				.add("TYPE_OF_PROGRAM", type_of_program)
+//				.add("ARTIST_NAME", artist_name)		
+//				.add("PROOF_TYPE", proof_type)
+//				.add("DATE_OF_PROGRAM", date_of_program)
+//				.add("VENUE", venue)
+//				.add("VENUETYPE", venuetype)
+//				.add("VENUECAPACITY", venuecapacity)
+//				.add("STREET1", street1)
+//				.add("CITY", city)
+//				.add("PROVINCE", province)
+//				.add("POSTAL_CODE", postal_code)
+//				.add("COUNTRY", country)
+//				.add("VENUE_PHONE", venue_phone)
+//				.add("VENUE_WEBSITE", venue_website)
+//				.add("PERFORMANCE_TIME", performance_time)
+//				.add("PROMOTER", promoter)
+//				.add("PROMOTER_STREET1", promoter_street1)
+//				.add("PROMOTER_CITY", promoter_city)
+//				.add("PROMOTER_PROVINCE", promoter_province)
+//				.add("PROMOTER_POSTAL_CODE", promoter_postal_code)
+//				.add("PROMOTER_COUNTRY", promoter_country)
+//				.add("PROMOTER_TELEPHONE", promoter_telephone)
+//				.add("compositions", arrayBuilder)		    		  
+//				.build();
+//
+//		System.out.println(value.toString());
 	}
 }
